93a37794dea654f581cb7049a53fe1035a242bd6
TTaket <575500603@qq.com>
2023-10-15 02:36:16 +0800

    finish : Lab4

--- src/network_interface.hh
+++ src/network_interface.hh
@@ -8,6 +8,7 @@
 #include <list>
 #include <optional>
 #include <queue>
+#include <map>
 #include <unordered_map>
 #include <utility>
 
@@ -35,12 +36,34 @@
 class NetworkInterface
 {
 private:
+  //现在的时间
+  uint64_t now_time_t;
+  //第三层以及以下不需要维护可靠性
+  // arp映射表 IP - eth
+  std::unordered_map<uint32_t , EthernetAddress>arp_ip2eth_map_;
+
+  //arp表有效时间
+  std::unordered_map<uint32_t , uint64_t>arp_valid_time_map_;
+
+  //arpReq存活 - 防止大量arp请求发送
+  std::unordered_map<uint32_t, uint64_t> _arpreq_survival_map_;
+
+  // 当时发送的时候不知道对方具体eth地址 发送arp请求 在这个时候把包放入此队列
+  // 当接受arp回复确定对方eth的时候（recv）把这个等待队列对应信息发送出去
+  std::unordered_map<uint32_t , std::vector<InternetDatagram> >wait_map;
+  // 出站队列 - 已经准备好了的
+  std::deque<EthernetFrame>out_que_;
+
   // Ethernet (known as hardware, network-access, or link-layer) address of the interface
   EthernetAddress ethernet_address_;
 
   // IP (known as Internet-layer or network-layer) address of the interface
   Address ip_address_;
 
+  // ARPMAP时间
+  const uint64_t MaxArpMapTime;
+  // ARPREQ时间
+  const uint64_t MaxArpReqTime;
 public:
   // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
   // addresses
@@ -54,6 +77,7 @@ public:
   // for the next hop.
   // ("Sending" is accomplished by making sure maybe_send() will release the frame when next called,
   // but please consider the frame sent as soon as it is generated.)
+  // 转发 - 转发同样负责超时管理
   void send_datagram( const InternetDatagram& dgram, const Address& next_hop );
 
   // Receives an Ethernet frame and responds appropriately.
